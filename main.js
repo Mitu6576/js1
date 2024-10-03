6a28297c08a3a534ce9224fced7a42751e0c62b7
Mitu6576 <mitumitu6576@gmail.com>
2024-10-03 18:33:08 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,101 @@
+// let x = 10;
+// let y = 20;
+// let z= x * y;
+// console.log( z );
+// output:200
+
+
+// math No.1
+
+let sara_has_apples = 25;
+
+let sara_buy_more = 15;
+ 
+let total_apples = sara_has_apples + sara_buy_more;
+
+console.log( 'sara has total ' + total_apples +' apples' );
+
+// output:sara has total 40 apples
+
+// math no.2
+
+let Jon_had = 500;
+
+let Spend = 200;
+ 
+let jon_has = Jon_had - Spend;
+
+console.log( ' Jon has ' + jon_has + ' taka');
+
+// output: Jon has 300 taka
+
+// math no.3
+
+let per_hour_machine_make = 120;
+
+let total_hour = 8;
+
+let total_machine_make = per_hour_machine_make *  total_hour;
+
+console.log( total_machine_make + ' machine will be make')
+
+// output:960 machine will be make.
+
+// math no.4
+
+let per_row_tree = 12;
+
+let total_row = 8;
+let add_tree = 20;
+
+let total_tree = per_row_tree * total_row;
+let after_add_tree= total_tree + add_tree
+
+console.log( 'total tree ' + after_add_tree );
+
+// output:total tree 116.
+
+// math no.5
+
+let rahim_has_box = 5;
+let per_box_orange = 10; 
+let send_freind_orange = 12;
+let total_orange = rahim_has_box * per_box_orange;
+let after_send_total_orange = total_orange - send_freind_orange;
+console.log( 'rahim has ' + after_send_total_orange + ' orange');
+
+// output:rahim has 38 orange.
+
+// math no.6
+let train = 150;
+let time = 3;
+let per_hour = train / time;
+console.log(per_hour + ' ' +'kilometers ' );
+
+// output: 50 kilometers
+ 
+
+// math no.7
+
+let mechine = 6;
+let per_day_products = 200;
+let total_machine = ( 6+2 );
+let not_sell = 3;
+let per_machine_products = per_day_products / mechine;
+let total_products = per_machine_products * total_machine;
+let remaining_products = total_products % not_sell;
+console.log( remaining_products + ' ' + 'products remain');
+//  output: 263.6666666666667 products remain.
+
+
+//  math no:8
+
+let chocoletes =120;
+let packets = 9;
+let chocoletes_remain = chocoletes % packets;
+console.log( chocoletes_remain + ' ' + 'chocoletes remain');
+
+// output
+
+
+
